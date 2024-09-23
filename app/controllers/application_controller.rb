4fd14f9ff289daea21a32d0da6899dfab82f93b8
Koshi2277 <saikisyligazbrhx9705@gmail.com>
2024-09-23 21:43:21 +0900

    ログイン機能がほぼ完成した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    # Deviseのリダイレクト先を指定
+    def after_sign_in_path_for(resource)
+        root_path  # ホーム画面にリダイレクト
+      end
+  
+      def after_sign_out_path_for(resource_or_scope)
+        root_path  # ログアウト後もホーム画面にリダイレクト
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
      get 'aecsites/index' => 'aecsites#index'
-     root 'aecsites#index'
+     root 'aecsites#index' #ログイン機能でdeviseを使う時にこのコードは必須、なぜならログインした後のシーン遷移を決めないとroot_pathに移動してしまうから
   resources :aecsites do
      
      collection do
